ef6bc44dbeaa8589b5363f6b115eedf4df9474d5
nicolasberretta <neberretta@mail.austral.edu.ar>
2025-03-16 22:09:34 -0300

    works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:3030").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<String> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    let terms = handle_request(&http_request[0]).unwrap();// handle the first value in the request -> "GET /pi/1 HTTP/1.1"
+    let pi = liebniz_series(terms);
+    let body = format!(
+        "The value of pi approximated using {} terms is: {:.15}",
+        terms, pi
+    );
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        body.len(),
+        body
+    );
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn handle_request(request_line: &str) -> Option<usize> {// TODO: use result maybe
+    let parts: Vec<&str> = request_line.split_whitespace().collect();
+
+    if parts.len() < 3 { // parts should be like: "GET /pi/1 HTTP/1.1"
+        return None;// if so, i am missing something
+    }
+
+    if parts[0] != "GET" {
+        return None;
+    }
+
+    let path = parts[1]; // /pi/1
+
+    if path.starts_with("/pi/") {
+        if let Some(terms_str) = path.strip_prefix("/pi/") {
+            return terms_str.parse::<usize>().ok();
+        }
+    }
+    None
+}
+
+fn liebniz_series(terms: usize) -> f64 {
+    let mut sum = 0.0;
+
+    for i in 0..terms {
+        let term = (-1.0_f64).powi(i as i32) / (2 * i + 1) as f64;
+        sum += term;
+    }
+    4.0 * sum
 }
