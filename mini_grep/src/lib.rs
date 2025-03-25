9b4579a88ab4cd0bc1bb987ff9dbefeb0ddc7aa1
nicolasberretta <neberretta@mail.austral.edu.ar>
2025-03-24 21:29:58 -0300

    added seq and conc search for mini_grep tp

--- mini_grep/src/lib.rs
+++ mini_grep/src/lib.rs
@@ -1,14 +1,100 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fs;
+use std::error::Error;
+use std::thread;
+use std::sync::Arc;
+
+#[derive(Clone)]
+pub struct Config {
+    pub method: String,
+    pub query: String,
+    pub file_paths: Box<[String]>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let method = args[1].clone();
+        let query = args[2].clone();
+        let file_paths = args[3..].to_vec().into_boxed_slice();
+
+        Ok(Config { method, query, file_paths })
     }
 }
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match config.method.as_str() {
+        "seq" => sequential_search(config),
+        "conc" => concurrency_search(config),
+//        "c-chunk" => c_chuck_search(config),
+        _ => Err("Invalid method".into())
+
+    }
+}
+
+fn sequential_search(config: Config) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let _ = config.file_paths
+        .iter()
+        .try_for_each(|file_path| {
+            let contents = fs::read_to_string(file_path)?;
+            handle_file(config.query.as_str(),&contents);
+
+            Ok::<(), Box<dyn Error>>(())
+        });
+    Ok(())
+}
+
+fn concurrency_search(config: Config) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let query = Arc::new(config.query);
+
+    let handles: Vec<_> = config.file_paths
+        .as_ref()
+        .iter()
+        .map(|file_path| {
+            let query = Arc::clone(&query);
+            let file_path = file_path.clone();
+
+            thread::spawn(move || {
+                let contents = fs::read_to_string(file_path)?;
+                handle_file(&query, &contents);
+                Ok::<(), Box<dyn Error + Send + Sync>>(())
+            })
+        })
+        .collect();
+
+    for handle in handles {
+        if let Err(err) = handle.join().unwrap() {
+            return Err(err);
+        }
+    }
+
+    Ok(())
+}
+
+// fn c_chuck_search(config: Config) -> Result<(), Box<dyn Error + Send + Sync>> {
+
+// }
+
+fn handle_file(query: &str, contents: &str) {
+    contents
+        .lines()
+        .for_each(|line| {
+            if line.contains(query) {println!("{}",line)}
+        });
+}
+
+// cargo run -- <seq/conc/c-chunk> <word> <[book.txt, book.txt, book.txt, book.txt]>
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
