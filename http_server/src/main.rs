7047fc614cca587c4622dc400171781cd6fce7c8
nicolasberretta <neberretta@mail.austral.edu.ar>
2025-04-01 11:45:35 -0300

    add first iteration of thread_pool

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,6 +1,25 @@
 mod server;
 mod math;
+mod thread_pool;
+mod request_handler;
+
+use std::env;
+
 
 fn main() {
-    server::start_server()
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <mode>", args[0]);
+        eprintln!("Modes: multi, pool");
+        return;
+    }
+
+    let mode = &args[1];
+
+    match mode.as_str() {
+        "multi" => server::start_server(),
+        "pool" => thread_pool::start_server(),
+        _ => eprintln!("Invalid mode! Use 'multi' or 'pool'"),
+    }
 }
